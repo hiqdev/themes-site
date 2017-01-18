08bf56aa29d07ae184965687fe78a70bc06a30e7
tofid <andreyklochok@gmail.com>
2017-01-18 20:02:11 +0200

    Improved action catalog

--- src/controllers/ThemeController.php
+++ src/controllers/ThemeController.php
@@ -2,10 +2,23 @@
 
 namespace hiqdev\themes\site\controllers;
 
+use hiqdev\themes\site\repositories\ThemeRepository;
+use yii\data\ArrayDataProvider;
 use yii\web\Controller;
 
 class ThemeController extends Controller
 {
+    /**
+     * @var ThemeRepository
+     */
+    private $themeRepository;
+
+    public function __construct($id, $module, ThemeRepository $themeRepository, $config = [])
+    {
+        $this->themeRepository = $themeRepository;
+        parent::__construct($id, $module, $config = []);
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
@@ -13,6 +26,13 @@ class ThemeController extends Controller
 
     public function actionCatalog()
     {
-        return $this->render('catalog');
+        $models = $this->themeRepository->getThemes();
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $models,
+        ]);
+
+        return $this->render('catalog', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 }
