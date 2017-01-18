248f260e485b103b9cfb4ca129227366521b38dd
tofid <andreyklochok@gmail.com>
2017-01-18 20:01:38 +0200

    Added theme model

---
+++ src/models/Theme.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace hiqdev\themes\site\models;
+
+use Yii;
+use yii\base\Model;
+
+class Theme extends Model
+{
+    public $label;
+    public $name;
+    public $description;
+    public $screenshot;
+
+    public function attributeLabels()
+    {
+        return [
+            'label' => Yii::t('hiqdev:themes', 'Label'),
+            'name' => Yii::t('hiqdev:themes', 'Name'),
+            'description' => Yii::t('hiqdev:themes', 'Description'),
+            'screenshot' => Yii::t('hiqdev:themes', 'Screenshot'),
+        ];
+    }
+}
