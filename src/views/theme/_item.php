537b8d273e20ec9c5c4e68c691bb240b12a0f159
tofid <andreyklochok@gmail.com>
2017-01-20 15:23:12 +0200

    Added list product view

---
+++ src/views/theme/_item.php
@@ -0,0 +1,26 @@
+<div class="product">
+    <div class="flip-container">
+        <div class="flipper">
+            <div class="front">
+                <a href="detail.html">
+                    <img src="img/product1.jpg" alt="" class="img-responsive">
+                </a>
+            </div>
+            <div class="back">
+                <a href="detail.html">
+                    <img src="img/product1_2.jpg" alt="" class="img-responsive">
+                </a>
+            </div>
+        </div>
+    </div>
+    <a href="detail.html" class="invisible">
+        <img src="img/product1.jpg" alt="" class="img-responsive">
+    </a>
+    <div class="text">
+        <h3><a href="detail.html"><?= $model->label ?></a></h3>
+        <p class="price">$143.00</p>
+        <p class="buttons">
+            <a href="detail.html" class="btn btn-default">View detail</a>
+        </p>
+    </div>
+</div>
