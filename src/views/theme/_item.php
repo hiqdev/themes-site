73db963e82943c28f8db0c465b796487f6648de6
tofid <andreyklochok@gmail.com>
2017-01-20 15:42:47 +0200

    Improved _item view

--- src/views/theme/_item.php
+++ src/views/theme/_item.php
@@ -1,3 +1,11 @@
+<?php
+
+/** @var \hiqdev\themes\site\models\Theme $model */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+?>
 <div class="product">
     <div class="flip-container">
         <div class="flipper">
@@ -13,14 +21,13 @@
             </div>
         </div>
     </div>
-    <a href="detail.html" class="invisible">
+    <a href="<?= Url::to($model->link) ?>" class="invisible">
         <img src="img/product1.jpg" alt="" class="img-responsive">
     </a>
     <div class="text">
-        <h3><a href="detail.html"><?= $model->label ?></a></h3>
-        <p class="price">$143.00</p>
+        <h3><?= Html::a($model->label, $model->link) ?></h3>
         <p class="buttons">
-            <a href="detail.html" class="btn btn-default">View detail</a>
+            <?= Html::a(Yii::t('hiqdev:themes', 'View detail'), $model->link, ['class' => 'btn btn-default']) ?>
         </p>
     </div>
 </div>
