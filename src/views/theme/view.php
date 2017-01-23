069aa8a1897206e4f7aa7705bf9cca22c94afd2d
tofid <andreyklochok@gmail.com>
2017-01-23 18:39:08 +0200

    Added images

--- src/views/theme/view.php
+++ src/views/theme/view.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="row" id="productMain">
         <div class="col-sm-6">
             <div id="mainImage">
-                <img src="img/detailbig2.jpg" alt="" class="img-responsive">
+                <?= Html::img($model->image, ['class' => 'img-responsive']) ?>
             </div>
 
             <div class="ribbon sale">
@@ -33,7 +33,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-sm-6">
             <div class="box">
                 <h1 class="text-center"><?= $model->label ?></h1>
-                <p class="goToDescription"><a href="#details" class="scroll-to">Scroll to product details, material &amp; care and sizing</a>
+                <p class="goToDescription"><a href="#details" class="scroll-to">Scroll to product details, material
+                        &amp; care and sizing</a>
                 </p>
                 <p class="price">$124.00</p>
 
@@ -41,27 +42,18 @@ $this->params['breadcrumbs'][] = $this->title;
                     <a href="basket.html" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Add to cart</a>
                     <a href="basket.html" class="btn btn-default"><i class="fa fa-heart"></i> Add to wishlist</a>
                 </p>
-
-
             </div>
-
-            <div class="row" id="thumbs">
-                <div class="col-xs-4">
-                    <a href="img/detailbig1.jpg" class="thumb">
-                        <img src="img/detailsquare.jpg" alt="" class="img-responsive">
-                    </a>
+            <?php if ($model->images) : ?>
+                <div class="row" id="thumbs">
+                    <?php foreach ($model->images as $src) : ?>
+                        <div class="col-xs-4">
+                            <a href="<?= $src ?>" class="thumb">
+                                <?= Html::img($src, ['class' => 'img-responsive']) ?>
+                            </a>
+                        </div>
+                    <?php endforeach ?>
                 </div>
-                <div class="col-xs-4">
-                    <a href="img/detailbig2.jpg" class="thumb">
-                        <img src="img/detailsquare2.jpg" alt="" class="img-responsive">
-                    </a>
-                </div>
-                <div class="col-xs-4">
-                    <a href="img/detailbig3.jpg" class="thumb active">
-                        <img src="img/detailsquare3.jpg" alt="" class="img-responsive">
-                    </a>
-                </div>
-            </div>
+            <?php endif ?>
         </div>
     </div>
 
@@ -73,9 +65,12 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="social">
             <h4><?= Yii::t('hiqdev:themes', 'Show it to your friends') ?></h4>
             <p>
-                <a href="#" class="external facebook" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-facebook"></i></a>
-                <a href="#" class="external gplus" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-google-plus"></i></a>
-                <a href="#" class="external twitter" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="external facebook" data-animate-hover="pulse" style="opacity: 1;"><i
+                        class="fa fa-facebook"></i></a>
+                <a href="#" class="external gplus" data-animate-hover="pulse" style="opacity: 1;"><i
+                        class="fa fa-google-plus"></i></a>
+                <a href="#" class="external twitter" data-animate-hover="pulse" style="opacity: 1;"><i
+                        class="fa fa-twitter"></i></a>
                 <a href="#" class="email" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-envelope"></i></a>
             </p>
         </div>
