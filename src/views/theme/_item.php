6c412301ea65ddaf64b8b09d0721c1a4e0382efb
tofid <andreyklochok@gmail.com>
2017-01-23 18:38:31 +0200

    Added images, improved view

--- src/views/theme/_item.php
+++ src/views/theme/_item.php
@@ -5,24 +5,25 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+
 ?>
 <div class="product">
     <div class="flip-container">
         <div class="flipper">
             <div class="front">
-                <a href="detail.html">
-                    <img src="img/product1.jpg" alt="" class="img-responsive">
+                <a href="<?= Url::to($model->link) ?>">
+                    <?= Html::img($model->image, ['class' => 'img-responsive']) ?>
                 </a>
             </div>
             <div class="back">
-                <a href="detail.html">
-                    <img src="img/product1_2.jpg" alt="" class="img-responsive">
+                <a href="<?= Url::to($model->link) ?>">
+                    <?= Html::img($model->image, ['class' => 'img-responsive']) ?>
                 </a>
             </div>
         </div>
     </div>
     <a href="<?= Url::to($model->link) ?>" class="invisible">
-        <img src="img/product1.jpg" alt="" class="img-responsive">
+        <?= Html::img($model->image, ['class' => 'img-responsive']) ?>
     </a>
     <div class="text">
         <h3><?= Html::a($model->label, $model->link) ?></h3>
