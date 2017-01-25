d8aef04653f9e1503c63415fcf325eb0d592ba9c
tofid <andreyklochok@gmail.com>
2017-01-25 16:46:33 +0200

    Added Menu widget for theme types

--- src/views/theme/catalog.php
+++ src/views/theme/catalog.php
@@ -1,6 +1,8 @@
 <?php
 
+use hiqdev\themes\site\models\Theme;
 use yii\widgets\ListView;
+use yii\widgets\Menu;
 
 $this->title = Yii::t('hiqdev:themes', 'Catalog');
 $this->params['breadcrumbs'][] = $this->title;
@@ -8,56 +10,20 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="col-md-3">
+
     <div class="panel panel-default sidebar-menu">
 
         <div class="panel-heading">
-            <h3 class="panel-title">Categories</h3>
+            <h3 class="panel-title"><?= Yii::t('hiqdev:themes', 'Template types') ?></h3>
         </div>
 
         <div class="panel-body">
-            <ul class="nav nav-pills nav-stacked category-menu">
-                <li>
-                    <a href="category.html">Men <span class="badge pull-right">42</span></a>
-                    <ul>
-                        <li><a href="category.html">T-shirts</a>
-                        </li>
-                        <li><a href="category.html">Shirts</a>
-                        </li>
-                        <li><a href="category.html">Pants</a>
-                        </li>
-                        <li><a href="category.html">Accessories</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="active">
-                    <a href="category.html">Ladies  <span class="badge pull-right">123</span></a>
-                    <ul>
-                        <li><a href="category.html">T-shirts</a>
-                        </li>
-                        <li><a href="category.html">Skirts</a>
-                        </li>
-                        <li><a href="category.html">Pants</a>
-                        </li>
-                        <li><a href="category.html">Accessories</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="category.html">Kids  <span class="badge pull-right">11</span></a>
-                    <ul>
-                        <li><a href="category.html">T-shirts</a>
-                        </li>
-                        <li><a href="category.html">Skirts</a>
-                        </li>
-                        <li><a href="category.html">Pants</a>
-                        </li>
-                        <li><a href="category.html">Accessories</a>
-                        </li>
-                    </ul>
-                </li>
-
-            </ul>
-
+            <?= Menu::widget([
+                'items' => Theme::getTypesMenuItems(),
+                'options' => [
+                    'class' => 'nav nav-pills nav-stacked category-menu',
+                ],
+            ]) ?>
         </div>
     </div>
 </div>
